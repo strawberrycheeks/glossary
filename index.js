69f257316d5c721d81dbdae5e313fedc3ad59bad
Ekaterina <abramushkinakatya@gmail.com>
2025-03-04 01:07:12 +0700

    Добавит код для серверной части

---
+++ index.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+let terms = [];
+
+const downloadTerms = async () => {
+  try {
+    const response = await axios.get(
+      "https://raw.githubusercontent.com/strawberrycheeks/web-glossary/refs/heads/main/dictionary.json"
+    );
+    terms = response.data.terms;
+  } catch (error) {
+    console.error("При загрузке терминов произошла ошибка:", error);
+  }
+};
+
+const expressApp = express();
+expressApp.use(
+  express.static(path.join(__dirname, "public"), { index: false })
+);
+
+expressApp.get("/", (_, response) => {
+  response.send(
+    'Глоссарий для выпускной квалификационной работы на тему "Исследование особенностей моделей машинного обучения с использованием библиотеки TensorFlow.js"'
+  );
+});
+
+expressApp.get("/mindmap", (_, response) => {
+  response.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+expressApp.get("/terms", (_, response) => {
+  response.json(terms);
+});
+
+expressApp.get("/terms/:id", (request, response) => {
+  const termId = request.params.id;
+  const term = terms.find((t) => t.id === termId);
+
+  if (term) {
+    response.json(term);
+  } else {
+    response.status(404).send(`По запросу ${termId} термин не найден`);
+  }
+});
+
+expressApp.listen(3000, async (error) => {
+  if (error) {
+    console.error("При запуске сервера произошла ошибка:", error);
+    return;
+  }
+
+  await downloadTerms();
+  console.log(
+    `Сервер успешно запущен и доступен по адресу: http://localhost:${3000}`
+  );
+});
